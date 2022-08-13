d4187704a010cf0ec6c54d83872d6d0274d56995
Taras <sokha2411@gmail.com>
2022-08-13 14:39:43 +0300

    added markup for counter

---
+++ src/app/my-counter/my-counter.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {Observable} from "rxjs";
+
+@Component({
+  selector: 'app-my-counter',
+  templateUrl: './my-counter.component.html',
+  styleUrls: ['./my-counter.component.css']
+})
+export class MyCounterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
