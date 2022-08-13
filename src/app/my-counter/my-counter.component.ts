6273014a2fd773d0700ae6e92811cffe04173c8f
Taras <sokha2411@gmail.com>
2022-08-13 15:02:30 +0300

    added counter and reducer of NgRx library

--- src/app/my-counter/my-counter.component.ts
+++ src/app/my-counter/my-counter.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable} from "rxjs";
+import { Store } from '@ngrx/store';
+import { increment,decrement,reset } from "../NgRx/counter.action";
 
 @Component({
   selector: 'app-my-counter',
